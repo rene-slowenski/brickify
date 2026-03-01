6ab5099ff8258dbca40533f93e507e11ed93b8be
rene@slowenski.de <rene@slowenski.de>
2026-03-01 14:41:57 +0100

    Fix typo

--- brickify/cli.py
+++ brickify/cli.py
@@ -1,23 +1,38 @@
-'''Command line interface to Brickify'''
+"""Command line interface to Brickify"""
+
 import click
 import brickify
 from brickify import palettes
 
 
 @click.command()
-@click.argument('image', required=True, type=click.Path(dir_okay=False,
-                                                        exists=True,
-                                                        resolve_path=True))
-@click.argument('output', default=None, required=False,
-                type=click.Path(resolve_path=True))
-@click.option('--size', default=None, type=int,
-              help='Number of bricks the longest side of the brickyfied image should have.')
-@click.option('--dither/--no-dither', default=False,
-              help='Use dither algorithm to spread the color approximation error.')
-@click.option('--palette', default=None,
-              type=click.Choice(palettes.bricks().keys()),
-              help='Palette to use based on real brick colors.')
+@click.argument(
+    "image",
+    required=True,
+    type=click.Path(dir_okay=False, exists=True, resolve_path=True),
+)
+@click.argument(
+    "output", default=None, required=False, type=click.Path(resolve_path=True)
+)
+@click.option(
+    "--size",
+    default=None,
+    type=int,
+    help="Number of bricks the longest side of the brickified image should have.",
+)
+@click.option(
+    "--dither/--no-dither",
+    default=False,
+    help="Use dither algorithm to spread the color approximation error.",
+)
+@click.option(
+    "--palette",
+    default=None,
+    type=click.Choice(palettes.bricks().keys()),
+    help="Palette to use based on real brick colors.",
+)
 def main(image, output, size, palette, dither):
-    '''Brickify an image!'''
-    brickify.main(image, output_path=output, size=size,
-                palette_mode=palette, dither=dither)
+    """Brickify an image!"""
+    brickify.main(
+        image, output_path=output, size=size, palette_mode=palette, dither=dither
+    )
