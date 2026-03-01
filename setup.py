c327569a7d08669129191216e10030b7a18f38f8
rene@slowenski.de <rene@slowenski.de>
2026-03-01 14:29:43 +0100

    Code cleanup

--- setup.py
+++ setup.py
@@ -9,25 +9,27 @@ setup(
     author_email="rene@slowenski.de",
     url="https://github.com/rene-slowenski/brickify",
     description="Make images look as if they are made out of 1x1 interlocking bricks",
-    long_description=("Brickify is a python program that takes a static image or"
-                      " gif and makes it so that it looks as if it was built "
-                      "out of interlocking bricks."),
+    long_description=(
+        "Brickify is a python program that takes a static image or"
+        " gif and makes it so that it looks as if it was built "
+        "out of interlocking bricks."
+    ),
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python',
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
     ],
     license="MIT",
-    packages=['brickify'],
-    install_requires=['pillow', 'click'],
+    packages=["brickify"],
+    install_requires=["pillow", "click"],
     include_package_data=True,
     entry_points={
-        'console_scripts': [
-            'brickify = brickify.cli:main',
+        "console_scripts": [
+            "brickify = brickify.cli:main",
         ],
     },
     package_data={
-        'bricks': ['*.png'],
+        "bricks": ["*.png"],
     },
     test_suite="nose.collector",
-    tests_require=['nose'],
+    tests_require=["nose"],
 )
--- tests/test_brickify.py
+++ tests/test_brickify.py
@@ -1,4 +1,5 @@
-'''Unit tests for brickify'''
+"""Unit tests for brickify"""
+
 # They can be run individually, for example:
 # nosetests tests.test_brickify:Create.test_brickify_image
 import os
@@ -9,11 +10,11 @@ from PIL import Image
 import brickify
 
 TEST_DIR = os.path.realpath(os.path.dirname(__file__))
-FLOWER_PATH = os.path.join(TEST_DIR, '..', 'brickify', 'assets', 'flower.jpg')
+FLOWER_PATH = os.path.join(TEST_DIR, "..", "brickify", "assets", "flower.jpg")
 
 
 class Create(unittest.TestCase):
-    '''Unit tests that create files.'''
+    """Unit tests that create files."""
 
     def setUp(self):
         self.out_path = None
@@ -23,47 +24,52 @@ class Create(unittest.TestCase):
             os.remove(self.out_path)
 
     def create_tmpfile(self, suffix):
-        '''Creates a temporary file and stores the path in self.out_path'''
-        handle, self.out_path = tempfile.mkstemp(prefix='brick_', suffix=suffix)
+        """Creates a temporary file and stores the path in self.out_path"""
+        handle, self.out_path = tempfile.mkstemp(prefix="brick_", suffix=suffix)
         os.close(handle)
         self.assertTrue(os.path.exists(self.out_path))
         self.assertTrue(os.path.getsize(self.out_path) == 0)
 
     def test_brickify_image(self):
-        '''Can we brickify a static image?'''
-        self.create_tmpfile('.png')
-        self.assertTrue(os.path.exists(FLOWER_PATH),
-                        "Could not find image : {0}".format(FLOWER_PATH))
+        """Can we brickify a static image?"""
+        self.create_tmpfile(".png")
+        self.assertTrue(
+            os.path.exists(FLOWER_PATH),
+            "Could not find image : {0}".format(FLOWER_PATH),
+        )
 
         brickify.main(FLOWER_PATH, output_path=self.out_path)
         self.assertTrue(os.path.getsize(self.out_path) > 0)
 
     def test_brickify_gif(self):
-        '''Can we brickify a gif?'''
-        self.create_tmpfile('.gif')
-        gif_path = os.path.join(TEST_DIR, '..', 'brickify', 'assets', 'bacon.gif')
-        self.assertTrue(os.path.exists(gif_path),
-                        "Could not find image : {0}".format(gif_path))
+        """Can we brickify a gif?"""
+        self.create_tmpfile(".gif")
+        gif_path = os.path.join(TEST_DIR, "..", "brickify", "assets", "bacon.gif")
+        self.assertTrue(
+            os.path.exists(gif_path), "Could not find image : {0}".format(gif_path)
+        )
         brickify.main(gif_path, output_path=self.out_path)
         self.assertTrue(os.path.getsize(self.out_path) > 0)
 
     def test_brickify_palette(self):
-        '''Can we use a palette?'''
-        self.create_tmpfile('.png')
-        self.assertTrue(os.path.exists(FLOWER_PATH),
-                        "Could not find image : {0}".format(FLOWER_PATH))
+        """Can we use a palette?"""
+        self.create_tmpfile(".png")
+        self.assertTrue(
+            os.path.exists(FLOWER_PATH),
+            "Could not find image : {0}".format(FLOWER_PATH),
+        )
         out = self.out_path
-        brickify.main(FLOWER_PATH, output_path=out, palette_mode='solid')
-        brickify.main(FLOWER_PATH, output_path=out, palette_mode='transparent')
-        brickify.main(FLOWER_PATH, output_path=out, palette_mode='effects')
-        brickify.main(FLOWER_PATH, output_path=out, palette_mode='mono')
-        brickify.main(FLOWER_PATH, output_path=out, palette_mode='all')
+        brickify.main(FLOWER_PATH, output_path=out, palette_mode="solid")
+        brickify.main(FLOWER_PATH, output_path=out, palette_mode="transparent")
+        brickify.main(FLOWER_PATH, output_path=out, palette_mode="effects")
+        brickify.main(FLOWER_PATH, output_path=out, palette_mode="mono")
+        brickify.main(FLOWER_PATH, output_path=out, palette_mode="all")
         self.assertTrue(os.path.getsize(out) > 0)
 
     def test_bricks_parameter(self):
-        '''Can we specify the --brick parameter and is the file size
-           proportional?'''
-        self.create_tmpfile('.png')
+        """Can we specify the --brick parameter and is the file size
+        proportional?"""
+        self.create_tmpfile(".png")
         brickify.main(FLOWER_PATH, output_path=self.out_path, size=5)
         size5 = os.path.getsize(self.out_path)
         brickify.main(FLOWER_PATH, output_path=self.out_path, size=10)
@@ -72,34 +78,34 @@ class Create(unittest.TestCase):
         self.assertTrue(size10 > size5)
 
     def test_small_brick(self):
-        '''Test hitting the minimal brick size'''
-        self.create_tmpfile('.png')
+        """Test hitting the minimal brick size"""
+        self.create_tmpfile(".png")
         brickify.main(FLOWER_PATH, output_path=self.out_path, size=1)
         self.assertTrue(Image.open(self.out_path).size == (30, 30))
 
     def test_dither_without_palette(self):
-        '''Dithering without a palette should still work'''
-        self.create_tmpfile('.png')
+        """Dithering without a palette should still work"""
+        self.create_tmpfile(".png")
         brickify.main(FLOWER_PATH, output_path=self.out_path, dither=True)
         self.assertTrue(os.path.getsize(self.out_path) > 0)
 
 
 class Functions(unittest.TestCase):
-    '''Test the behaviour of individual functions'''
+    """Test the behaviour of individual functions"""
 
     def test_get_new_filename(self):
-        '''Test the default output filename generation'''
+        """Test the default output filename generation"""
         # Is the generated path in the same directory?
         new_path = brickify.get_new_filename(FLOWER_PATH)
-        self.assertTrue(os.path.dirname(FLOWER_PATH) ==
-                        os.path.dirname(new_path))
+        self.assertTrue(os.path.dirname(FLOWER_PATH) == os.path.dirname(new_path))
         # Is the generated path unique?
-        self.assertFalse(os.path.exists(new_path),
-                         "Should not find image : {0}".format(new_path))
+        self.assertFalse(
+            os.path.exists(new_path), "Should not find image : {0}".format(new_path)
+        )
         # Test default file extensions
-        self.assertTrue(new_path.endswith('_brick.jpg'))
-        new_path = brickify.get_new_filename(FLOWER_PATH, '.gif')
-        self.assertTrue(new_path.endswith('_brick.gif'))
+        self.assertTrue(new_path.endswith("_brick.jpg"))
+        new_path = brickify.get_new_filename(FLOWER_PATH, ".gif")
+        self.assertTrue(new_path.endswith("_brick.gif"))
 
     def test_brick_palette_structure(self):
         """Validate brick palettes structured in 3's."""
@@ -115,13 +121,16 @@ class Functions(unittest.TestCase):
 
 
 class Failures(unittest.TestCase):
-    '''Make sure things fail when they should'''
+    """Make sure things fail when they should"""
+
     def test_bad_image_path(self):
-        '''Test invalid image path'''
-        fake_path = os.path.join(TEST_DIR, 'fake_image.jpg')
-        self.assertFalse(os.path.exists(fake_path),
-                         "Should not find image : {0}".format(fake_path))
+        """Test invalid image path"""
+        fake_path = os.path.join(TEST_DIR, "fake_image.jpg")
+        self.assertFalse(
+            os.path.exists(fake_path), "Should not find image : {0}".format(fake_path)
+        )
         self.assertRaises(SystemExit, brickify.main, fake_path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
